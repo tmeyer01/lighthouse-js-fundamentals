18006838336a2437200203635d11e4187fdb7569
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-08-11 10:38:02 -0700

    Add funct with use of return

--- lighthouse-js-fundamentals/hello.js
+++ lighthouse-js-fundamentals/hello.js
@@ -1,3 +1,7 @@
+//Function that uses console to display Hello and the name of person
+
+/*
+
 const sayHello = (name) =>{
   console.log("Hello, " + name);
 }
@@ -5,3 +9,17 @@ const sayHello = (name) =>{
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+*/
+
+
+//Function that use return (Wont show up in console, unless console.log used on varible that stores value of the function )
+
+const returnSayHello = (name) =>{
+  return "Hello, " + name;
+
+}
+
+let greeting = returnSayHello('John');
+console.log(greeting);
+
