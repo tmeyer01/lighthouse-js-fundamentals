4227173db09b913722bd98915bbe45f4aaf4f825
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-08-11 17:48:59 -0700

    While loop to go through array

--- lighthouse-js-fundamentals/packing-list.js
+++ lighthouse-js-fundamentals/packing-list.js
@@ -3,6 +3,18 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("Kitchen stuff to pack:");
 
+
+//Using a for loop to console.log each item in array
+/*
 for(let i = 0; i < packingList.length; i++){
   console.log(packingList[i]);
+}
+*/
+
+
+//Below we have used a while loop to go through each item in array 
+let i = 0;
+while(i < packingList.length){
+  console.log(packingList[i]);
+  i++
 }
