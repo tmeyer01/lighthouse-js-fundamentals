68d221a71c47106505dd2dc130007387843a7fd4
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-08-11 17:27:40 -0700

    Sorting through array of ice cream flavours

---
+++ lighthouse-js-fundamentals/ice-cream.js
@@ -0,0 +1,25 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+//Displays all items in array 
+//console.log(iceCreamFlavours);
+
+
+
+
+
+//Using array push method to add "root beer" flavour to end of array
+iceCreamFlavours.push("root beer");
+
+//Displays all ice cream flavours in array
+console.log(iceCreamFlavours);
+
+
+//Displays First IceCream flavour
+console.log(iceCreamFlavours[0]);
+
+//Displays item 5 in array 
+console.log(iceCreamFlavours.slice(-1));
+
+//Display total number of items in array 
+
+console.log(iceCreamFlavours.length);
